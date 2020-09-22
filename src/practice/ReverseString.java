c233088c81760a5bd760894fe325a02f97e1b25f
sip <dspirin@me.com>
2020-09-22 19:59:18 -0400

    Practice: Reverse String

---
+++ src/practice/ReverseString.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        String newStr = "Hello World!";
+
+        int strLength = newStr.length();
+
+        for (int i = strLength - 1; i >= 0 ; i--) {
+
+            System.out.print(newStr.charAt(i));
+        }
+    }
+}
