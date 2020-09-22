182463daf2f4c9b8990c6cff29ef4be22bddddde
sip <dspirin@me.com>
2020-09-22 17:51:49 -0400

    Practice indexOf and length

---
+++ src/practice/length.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class length {
+
+    public static void main(String[] args) {
+
+        String newStr = "Hello World!";
+
+        int lengthOfStr = newStr.length();
+
+        System.out.println("Length of '" + newStr + "' is " + lengthOfStr + " characters");
+    }
+}
