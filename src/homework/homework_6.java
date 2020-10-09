99057e45010c2f5514a655d7216ffedd5c384463
sip <dspirin@me.com>
2020-10-09 11:58:34 -0400

    Homework 6 corrections

--- src/homework/homework_6.java
+++ src/homework/homework_6.java
@@ -2,6 +2,7 @@ package homework;
 
 public class homework_6 {
     public static void main(String[] args) {
+
         // case 1
         int[] testArr = {22, 11, 33, 44, 55};
         System.out.println("Average of the given array is: " + arrayAvg(testArr));
@@ -15,25 +16,20 @@ public class homework_6 {
     // 1. Create a method to find average of an int-array
     public static int arrayAvg(int[] arr) {
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
+        for (int i : arr) {
+            sum += i;
         }
-        int ave = sum / arr.length;
-        return ave;
+        return sum / arr.length;
     }
 
     // 2. Create a method (NO return) that will print the index of given name in the given array
     public static void indexOfName(String[] strArr, String name) {
-        int indexOfName = 0;
+        int indexOfName = -1;
         for (int i = 0; i < strArr.length; i++) {
             if (name.equalsIgnoreCase(strArr[i])) {
-                indexOfName += i;
+                indexOfName = i;
             }
         }
-        if (indexOfName == 0) {
-            System.out.println("not in the list: Error -1");
-        } else {
-            System.out.println("Index of the given name in the array is: " + indexOfName);
-        }
+        System.out.println("Index of the given name in the array is: " + indexOfName);
     }
 }
